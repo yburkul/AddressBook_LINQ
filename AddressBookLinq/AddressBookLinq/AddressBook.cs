6c088b47f7ef6aa79466cba8fc9a082d7dc34799
yburkul <yburkul123@gmail.com>
2022-04-28 22:16:51 +0530

    Added: Create data table

---
+++ AddressBookLinq/AddressBookLinq/AddressBook.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+
+namespace AddressBook_LINQ
+{
+    class AddressBook
+    {
+        public void CreateAddressBookDataTable()
+        { 
+            DataTable dataTable = new DataTable();
+        }
+    }
+}
---
+++ AddressBookLinq/AddressBookLinq/Program.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+using System.Data;
+
+namespace AddressBook_LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Address Book Using LINQ");
+        }
+    }
+}
